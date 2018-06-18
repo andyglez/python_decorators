9e6bc73063b6e32a98a97092898c1572fe1851fb
Andy Gonzalez <aglez9538@gmail.com>
2018-06-18 00:01:31 -0400

    Defined descriptor's getters

--- componentized.py
+++ componentized.py
@@ -1,11 +1,25 @@
 def componentized(cls):
-    class WrappedClass(object):
+    class WrappedClass:
         def __init__(self, *args):
-            self.__dict__['wrapped_instance'] = cls(*args)
-            self.__dict__['components'] = []
+            self.wrapped_instance = cls(*args)
+            self.components = []
             for i in cls.__dict__:
                 if type(cls.__dict__[i]) == Component:
                     self.components.append(i)
+
+        def __getattr__(self, name):
+            attr_list = self.__checkattr(name)
+            if len(attr_list) == 1:
+                return getattr(self.wrapped_instance, name)
+            return tuple(getattr(self.wrapped_instance, attr) for attr in attr_list)
+
+        def __checkattr(self, name):
+            ret = []
+            for comp in self.components:
+                if comp in name and not(comp in ret):
+                    ret.append(comp)
+            return ret
+
     return WrappedClass
 
 
@@ -22,3 +36,20 @@ class Component:
             raise AttributeError("Type \"{}\" do not match \"{}\".".format(type(value), self.type))
         else:
             self.value = value
+
+
+@componentized
+class Color:
+    r = Component(int)
+    g = Component(int)
+    b = Component(int)
+
+    def __init__(self, r, g, b):
+        self.r = r
+        self.g = g
+        self.b = b
+
+
+c = Color(255, 200, 160)
+print("\"{}\" \"{}\" \"{}\"".format(c.r, c.g, c.b))
+print(c.rb)
