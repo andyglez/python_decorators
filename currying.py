32030b5c90373136a007253d99391c6ec94e71ee
Andy Gonzalez <aglez9538@gmail.com>
2018-06-18 03:20:28 -0400

    Done with currying

--- currying.py
+++ currying.py
@@ -4,7 +4,15 @@ from inspect import signature
 def currying(func):
     def wrapper(*args):
         count = len(signature(func).parameters)
+        total_args = [x for x in args]
         if count == len(args):
             return func(*args)
-        print("Nothing")
+
+        def curried(*new_args):
+            for i in new_args:
+                total_args.append(i)
+            if len(total_args) < count:
+                return curried
+            return func(*tuple(x for x in total_args))
+        return curried
     return wrapper
